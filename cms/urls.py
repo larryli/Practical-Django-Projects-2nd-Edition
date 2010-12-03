48ea7842712671331675f1374f9590adb06e21ec
Gavin <gmcdonald@esgi.com>
2010-12-03 14:49:47 -0500

    added custom admin page for flatpages and tiny_mce to provide rich text editing

--- cms/urls.py
+++ cms/urls.py
@@ -6,6 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	(r'^admin/', include(admin.site.urls)),
+	(r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve',
+		{'document_root': '/home/gavin/Documents/Practical-Django-Projects-2nd-Edition/scripts/tiny_mce/'}),
 
 	(r'', include('django.contrib.flatpages.urls')),
 )
