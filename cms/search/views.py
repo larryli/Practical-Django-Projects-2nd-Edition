597c7bba3ff5410839e92626b552ea82c1f00e87
Gavin <gmcdonald@esgi.com>
2010-12-06 11:47:05 -0500

    added keywords to search results

--- cms/search/views.py
+++ cms/search/views.py
@@ -5,10 +5,14 @@ def search(request):
 
 	query = request.GET.get('q', '')
 
+	keyword_results = []
 	results = []
+
 	if query:
+		keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in=query.split()).distinct()
 		results = FlatPage.objects.filter(content__icontains=query)
 
 	return render_to_response('search/search.html',
 		{'query': query,
+		 'keyword_results': keyword_results,
 		 'results': results})
