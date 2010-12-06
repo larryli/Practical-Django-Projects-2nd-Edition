dc6f48579b023d3549865a53ba57285ca8564b8a
Gavin <gmcdonald@esgi.com>
2010-12-06 11:54:14 -0500

    automatically redirects if only one result for a keyword

--- cms/search/views.py
+++ cms/search/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.contrib.flatpages.models import FlatPage
 
@@ -9,7 +10,12 @@ def search(request):
 	results = []
 
 	if query:
+
 		keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in=query.split()).distinct()
+
+		if keyword_results.count() == 1:
+			return HttpResponseRedirect(keyword_results[0].get_absolute_url())
+
 		results = FlatPage.objects.filter(content__icontains=query)
 
 	return render_to_response('search/search.html',
