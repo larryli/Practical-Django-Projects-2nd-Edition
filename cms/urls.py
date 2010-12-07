aabd92be5e0a8508f7ee627b420489eff6a7a08b
Gavin <gmcdonald@esgi.com>
2010-12-07 12:56:05 -0500

    view for entry detail and added templates for weblog

--- cms/urls.py
+++ cms/urls.py
@@ -9,7 +9,9 @@ urlpatterns = patterns('',
 	(r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve',
 		{'document_root': '/home/gavin/Documents/Practical-Django-Projects-2nd-Edition/scripts/tiny_mce/'}),
 
-	(r'^search/$', 'cms.search.views.search'),
+	(r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'coltrane.views.entry_detail'),
 	(r'^weblog/$', 'coltrane.views.entries_index'),
+
+	(r'^search/$', 'cms.search.views.search'),
 	(r'', include('django.contrib.flatpages.urls')),
 )
