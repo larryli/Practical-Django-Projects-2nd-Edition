43437418ff51edf5f54d14599f000cb51535f7c6
Gavin <gmcdonald@esgi.com>
2010-12-07 13:14:15 -0500

    added 404 support for users that navigate to entries that don't exist

--- coltrane/views.py
+++ coltrane/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404, render_to_response
 from coltrane.models import Entry
 
 def entries_index(request):
@@ -7,11 +7,13 @@ def entries_index(request):
 def entry_detail(request, year, month, day, slug):
 
 	import datetime, time
+
 	date_stamp = time.strptime(year+month+day, '%Y%b%d')
 	pub_date = datetime.date(*date_stamp[:3])
-
-	return render_to_response('coltrane/entry_detail.html', {'entry': Entry.objects.get(
+	entry = get_object_or_404(Entry,
 		pub_date__year=pub_date.year,
 		pub_date__month=pub_date.month,
 		pub_date__day=pub_date.day,
-		slug=slug)})
+		slug=slug)
+
+	return render_to_response('coltrane/entry_detail.html', {'entry': entry})
