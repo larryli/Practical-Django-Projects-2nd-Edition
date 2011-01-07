295b99d991aed2686ad9acd87405dff48bfc9944
Gavin <gmcdonald@esgi.com>
2011-01-07 10:35:27 -0500

    added views for Category list and Category detail, haven't set urls.py to handle them yet nor are there templates availble for them

--- coltrane/views.py
+++ coltrane/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render_to_response
+
 from coltrane.models import Entry
+from coltrane.models import Category
 
 def entries_index(request):
 	return render_to_response('coltrane/entry_index.html', {'entry_list': Entry.objects.all()})
+
+def category_list(request):
+	return render_to_response('coltrane/category_list.html', {'object_list': Category.objects.all()})
+
+def category_detail(request, slug):
+
+	category = get_object_or_404(Category, slug=slug)
+	return render_to_response(
+		'coltrane/category_detail.html',
+		{'object_list': category.entry_set.all(),
+		 'category': category})
