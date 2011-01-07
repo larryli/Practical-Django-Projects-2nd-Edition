8342584b0fbb81833b9bce9bd90623f96fae417a
Gavin <gmcdonald@esgi.com>
2011-01-07 14:36:07 -0500

    I realize I probably should have removed entries_index when I set up generic views

--- coltrane/views.py
+++ coltrane/views.py
@@ -4,9 +4,6 @@ from django.views.generic.list_detail import object_list
 from coltrane.models import Entry
 from coltrane.models import Category
 
-def entries_index(request):
-	return render_to_response('coltrane/entry_index.html', {'entry_list': Entry.objects.all()})
-
 def category_detail(request, slug):
 
 	category = get_object_or_404(Category, slug=slug)
